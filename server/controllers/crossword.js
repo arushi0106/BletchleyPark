b4dd3114eea8e2480cbfd9291cb99e8e2ec88118
arushi0106 <arushi.agarwal999@gmail.com>
2021-12-15 00:54:10 +0530

    Validating data from user

--- server/controllers/crossword.js
+++ server/controllers/crossword.js
@@ -4,36 +4,45 @@ import clg from "crossword-layout-generator";
 
 export const createCrossword = async (req, res) => {
   console.log(req.body);
-  const data=req.body.clue;
+  const data = req.body.clue;
 
-const re = /\s*(?:;|$)\s*/
-const nameList = data.split(re)
-var myjson=[];
-nameList.forEach(myfun);
+  const re = /\s*(?:;|$)\s*/
+  const nameList = data.split(re)
+  var myjson = [];
+  nameList.forEach(myfun);
 
 
-function myfun(item, index)
-{
-  if(index!=nameList.length){
-  console.log(item);
-  const se = /\s*(?:,|$)\s*/
-  const x=item.split(se);
-  var input_json = 
-      {
-        clue: x[0],
-        answer:  x[1],
+  function myfun(item, index) {
+    if (index != nameList.length - 1) {
+      console.log(item);
+      const se = /\s*(?:,|$)\s*/
+      const x = item.split(se);
+      var ok = 1;
+      var letters = /^[A-Za-z]+$/;
+      if (x[1].match(letters)) {
+        console.log('correct');
+        ok = 1;
       }
-      myjson.push(input_json);
-  console.log(x[0]);
-  console.log(x[1]);
+      else {
+        ok = 0;
+        console.log('wrong');
+      }
+
+
+      if (ok) {
+        var input_json =
+        {
+          clue: x[0],
+          answer: x[1],
+        }
+        myjson.push(input_json);
+
+      }
     }
+  }
 
-}
-console.log(myjson);
-
-  // res.send('THIS WORKS');
-  console.log(myjson);
-  const layout = await clg.generateLayout(myjson);
-  console.log(layout);
-  res.send(layout);
-};
+  
+    const layout = await clg.generateLayout(myjson);
+    console.log(layout);
+    res.send(layout);
+  };
